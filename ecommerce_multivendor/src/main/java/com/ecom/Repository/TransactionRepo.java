a089051b25f923f9b8f416cecdb2f6f567d9a655
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-04-06 10:48:16 +0530

    Starting working with Transection service

---
+++ ecommerce_multivendor/src/main/java/com/ecom/Repository/TransactionRepo.java
@@ -0,0 +1,10 @@
+package com.ecom.Repository;
+
+import com.ecom.Entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepo extends JpaRepository<Transaction , Long> {
+
+}
