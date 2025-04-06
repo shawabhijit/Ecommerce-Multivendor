667a6d2ecbaaf04331b85df926f1aa36f7492521
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-04-06 11:30:30 +0530

    Working with Transaction service is completed

--- ecommerce_multivendor/src/main/java/com/ecom/Controller/TransactionController.java
+++ ecommerce_multivendor/src/main/java/com/ecom/Controller/TransactionController.java
@@ -1,4 +1,35 @@
 package com.ecom.Controller;
 
+import com.ecom.Entity.SellerEntity;
+import com.ecom.Entity.Transaction;
+import com.ecom.Exceptions.SellerException;
+import com.ecom.Service.SellerService;
+import com.ecom.Service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/transactions")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+    private final SellerService sellerService;
+
+    @GetMapping("/seller")
+    public ResponseEntity<?> getTransactionBySellerId(@RequestHeader("Authorization") String jwt) throws SellerException {
+        SellerEntity seller = sellerService.getSellerProfile(jwt);
+
+        List<Transaction> transactions = transactionService.getTransactionBySellerId(seller);
+        return ResponseEntity.ok().body(transactions);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllTransaction() throws SellerException {
+        List<Transaction> transactions = transactionService.getAllTransaction();
+        return ResponseEntity.ok().body(transactions);
+    }
 }
