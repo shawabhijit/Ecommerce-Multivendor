85986f5dded7aa11e23d85f99702ecd1347b6fcb
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-04-08 17:29:45 +0530

    feat: Frontend Architecture created using react vite

---
+++ ecommerce-multivendor-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-yellow-200 font-bold font-sans text-5xl">Wellcome to HikariHub</h1>
+    </>
+  )
+}
+
+export default App
