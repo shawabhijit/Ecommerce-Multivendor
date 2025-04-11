0c36a0270ba7d59704120957d169dfa2927fbe65
Abhijit Sahoo <shawabhijit370@gmail.com>
2025-04-11 09:26:51 +0530

    working with review page

--- ecommerce-multivendor-frontend/src/Customer/Components/Pages/Review/ReviewCard.tsx
+++ ecommerce-multivendor-frontend/src/Customer/Components/Pages/Review/ReviewCard.tsx
@@ -1,8 +1,21 @@
+import { Avatar, Box, Grid, ListItem } from '@mui/material'
 import React from 'react'
 
 const ReviewCard = () => {
   return (
-    <div>ReviewCard</div>
+    <div className='flex justify-between'>
+      <Grid container spacing={2} gap={3}>
+        <Grid>
+          <ListItem>xs=1</ListItem>
+          <Box>
+            <Avatar className='text-white' sx={{height:56, width:56,backgroundColor:'#9155FD'}}>
+              H
+            </Avatar>
+          </Box>
+        </Grid>
+
+      </Grid>
+    </div>
   )
 }
 
